daa7202361d37ea65198af7b0226ad9d3096a6b0
seanjib99 <78782873+seanjib99@users.noreply.github.com>
2021-03-17 18:56:05 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# write a python program to find sum of the first n positive integers.
+# sum= (n*(n+1)) / 2
+...
+
+#####################################
+
+#n= int(input("input a number: "))
+#sum_num=(n+(n+1)) /2
+#print(sum_num)
+
+#########################################################################
+
+list=(1,2,3,4,5,6,7,8,9,10)
+total=sum(list)
+print(f"the sum of given positive integer is {total} ")
